678d39a9022a02b8e5d7fb507cfd186793cd22ff
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-11 18:05:37 -0500

    index2 / iss_promised implement same functionality using promises

---
+++ index2.js
@@ -0,0 +1,14 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = passTimes => {
+  for (passTime of passTimes) {
+    console.log(`Next pass at ${Date(passTime.risetime)} for ${passTime.duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then(printPassTimes)
+  .catch(err => {
+    console.log('Something went wrong!');
+    console.log(err);
+  });
---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = body => {
+  const url = 'https://freegeoip.app/json/' + JSON.parse(body).ip;
+  return request(url);
+};
+
+const fetchISSFlyOverTimes = body => {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = 'http://api.open-notify.org/iss-pass.json?lat=' + latitude + '&lon=' + longitude;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = () => {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(data => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
